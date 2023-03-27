9ed486a1a3a170ec562331934c0b90e4fd9d87ec
gpffh20 <eunbin584836@gmail.com>
2023-03-27 10:18:48 +0900

    add gitignore models.py - add author, upload mkdir uploads for upload

---
+++ session/blog/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+class Blog(models.Model):
+    title = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    introduce = models.TextField()
+    author = models.CharField(max_length=6, default="")
+    upload = models.FileField(upload_to='blog/uploads/', null=True)
+
+    
+    def __str__(self):
+        return self.title
